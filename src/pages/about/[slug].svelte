30f34577ad74b3296198c6f7351ec3e2aa3a6d8c
Kiran Ojha <54209728+kiranojhanp@users.noreply.github.com>
2021-12-27 07:47:44 +0545

    Added slugs for page/id

---
+++ src/pages/about/[slug].svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	export let slug: string;
+</script>
+
+<h1>This is a <code>about/{slug}</code> page</h1>
---
+++ src/pages/home/[slug].svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	export let slug: string;
+</script>
+
+<h1>This is a <code>home/{slug}</code> page</h1>
