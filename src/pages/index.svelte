c61f80221bc620376358d8d0d03380fcc0b95f37
Kiran Ojha <54209728+kiranojhanp@users.noreply.github.com>
2022-01-03 13:27:47 +0545

    svelte transition

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,12 +1,52 @@
 <script lang="ts">
 	import { metatags } from "@roxi/routify";
-	import Hello from "@components/Hello.svelte";
+	import { fade, fly } from "svelte/transition";
+	import { elasticOut } from "svelte/easing";
 
-	metatags.title = "My Routify app";
-	metatags.description = "Description coming soon...";
+	metatags.title = "Home page";
+	metatags.description = "This is my homepage";
+
+	let visible = true;
+
+	function spin(node: Element, { duration }) {
+		return {
+			duration,
+			css: (t: number) => {
+				const eased = elasticOut(t);
+				return `
+					transform: scale(${eased}) rotate(${eased * 1080}deg);
+					color: hsl(
+						${Math.trunc(t * 360)},
+						${Math.min(100, 1000 - 1000 * t)}%,
+						${Math.min(50, 500 - 500 * t)}%
+					);`;
+			},
+		};
+	}
 </script>
 
-<h1 class="font-medium text-6xl text-pink-500">Svelte boilerplate</h1>
-<p>This is the first page of routify</p>
+<label>
+	<input type="checkbox" bind:checked={visible} />
+	visible
+</label>
 
-<Hello />
+{#if visible}
+	<div class="centered" out:fade in:fly={{ y: -200 }}>
+		<span>transitions!</span>
+	</div>
+{/if}
+
+<style>
+	.centered {
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
+	}
+
+	span {
+		position: absolute;
+		transform: translate(-50%, -50%);
+		font-size: 4em;
+	}
+</style>
